92a974707ce057ad391255c3c4393ac8d4663f48
ingvijonasson <contact@ingvijonasson.com>
2019-12-10 10:51:58 +0100

    Setup sidebar and map height

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <div id="app" class="master">
-    <Map />
+  <div id="app">
+    <div class="master">
+      <header class="site-header">
+        <h1>Swiss ai</h1>
+      </header>
+      <main>
+        <Map class="map" />
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
-import Map from '@/components/Map';
+import Map from "@/components/Map";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Map
   }
@@ -16,11 +23,29 @@ export default {
 </script>
 
 <style>
+html {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: #353535;
+}
+
 body {
   margin: 0;
 }
 
-.master {
+.master,
+.map {
   min-height: 100vh;
+}
+
+.master {
+  display: grid;
+  grid-template-columns: 300px 1fr;
+}
+
+.site-header {
+  padding: 1.5rem;
+  border-right: 8px solid #9995;
 }
 </style>
