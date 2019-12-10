e4085cd512e0d13d1b5e9e8e765b43f9ce381813
Jonas Niestroj <j.niestroj@protonmail.ch>
2019-12-10 10:26:54 +0100

    Added basic mapbox

---
+++ src/components/Map.vue
@@ -0,0 +1,26 @@
+<template>
+  <MglMap :accessToken="accessToken" :mapStyle="mapStyle" />
+</template>
+
+<script>
+import Mapbox from 'mapbox-gl';
+import { MglMap } from 'vue-mapbox';
+
+export default {
+  components: {
+    MglMap
+  },
+  data() {
+    return {
+      accessToken:
+        'pk.eyJ1Ijoiam9uYXNuaWVzdHJvaiIsImEiOiJjazN6bmt3dHowandwM21wMzcwc21vdjdxIn0.P496caPNw9SXrMl_GbzHdw', // your access token. Needed if you using Mapbox maps
+      mapStyle: 'mapbox://styles/mapbox/streets-v11' // your map style
+    };
+  },
+
+  created() {
+    // We need to set mapbox-gl library here in order to use it in template
+    this.mapbox = Mapbox;
+  }
+};
+</script>
